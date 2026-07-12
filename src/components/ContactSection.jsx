88995b594bfcc130b437af6a411ac65cb01248d8
gayatriDev02 <blossomjasmine02@gmail.com>
2026-07-12 10:20:45 +0530

    almost done

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -166,7 +166,7 @@ function ContactSection() {
   "
         >
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3473.5050545400613!2d77.725553375532!3d29.472459875216593!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390c1be948f1964d%3A0xd6584e6d82ee16ee!2sPalasa%20Hotel%20%26%20Resort!5e0!3m2!1sen!2sin!4v1783690102328!5m2!1sen!2sin"
+           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3473.505811708099!2d77.71282367532193!3d29.47243777521666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390c1b6cb9afac5b%3A0xa0326992a00f5bc9!2sRadiant%20INN%20Hotels%20and%20Venue!5e0!3m2!1sen!2sin!4v1783831785447!5m2!1sen!2sin"
             width="100%"
             height="100%"
             style={{ border: 0 }}
@@ -174,8 +174,17 @@ function ContactSection() {
             loading="lazy"
             referrerPolicy="strict-origin-when-cross-origin"
           ></iframe>
+
+          {/* <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3473.505811708099!2d77.71282367532193!3d29.47243777521666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390c1b6cb9afac5b%3A0xa0326992a00f5bc9!2sRadiant%20INN%20Hotels%20and%20Venue!5e0!3m2!1sen!2sin!4v1783831785447!5m2!1sen!2sin"
+            width="600"
+            height="450"
+            style="border:0;"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="strict-origin-when-cross-origin"
+          ></iframe> */}
         </div>
-        
       </div>
     </section>
   );
